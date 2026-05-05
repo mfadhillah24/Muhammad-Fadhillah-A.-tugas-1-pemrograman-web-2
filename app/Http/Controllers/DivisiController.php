7258606ea11b90b84cad5e21f910aa18722e7dcf
mfadhillah <mfadhillah24@mhs.unitama.ac.id>
2026-05-05 11:00:03 +0800

    feat: implementasi form tambah data Divisi

--- app/Http/Controllers/DivisiController.php
+++ app/Http/Controllers/DivisiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Divisi;
 use Illuminate\Http\Request;
 
+
 class DivisiController extends Controller
 {
     /**
@@ -22,15 +23,35 @@ class DivisiController extends Controller
      */
     public function create()
     {
-        //
+        return view('divisi.create', [
+            'divisis' => Divisi::all()
+        ]);
+
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+   public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'nama_divisi' => 'required|string|max:255',
+            'deskripsi' => 'required|string|max:255',
+            'ketua_divisi' => 'required|string|max:255',
+            
+        ],
+        [
+            'nama_divisi.required' => 'Nama Divisi harus diisi.',
+            'deskripsi.required' => 'Deskripsi harus diisi.',
+            'ketua_divisi.required' => 'Ketua Divisi harus diisi.',
+           
+        ]);
+
+        Divisi::create($validate);
+
+        
+        return redirect()->route('divisi.index')->withSuccess('Data divisi berhasil disimpan!');
+    
     }
 
     /**
---
+++ resources/views/divisi/create.blade.php
@@ -0,0 +1,56 @@
+<x-tampilan>
+    <x-slot:title>Tambah Data Divisi COS</x-slot>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center mt-4">
+                <img src="{{ asset('images/logo cos.png') }}" alt="" width="120" class="mb-4">
+                
+                <p class="fst-italic">Open Mind for The Future with Open Source</p>
+            </div>
+        </div>
+        <div class="container mt-4">
+            <h3 class="mb-3">Form Tambah Data Divisi</h3>
+            <form action="{{ route('divisi.store') }}" method="POST">
+            @csrf
+                <div class="mb-3">
+                    <label for="nama_divisi" class="form-label">Nama Divisi</label>
+                    <input type="text" class="form-control  @error('nama_divisi') is-invalid @enderror" id="nama_divisi" name="nama_divisi" value="{{ old('nama_divisi') }}">
+                    @error('nama_divisi')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        
+                    @enderror
+                </div>
+                
+            
+               
+                <div class="mb-3">
+                    <label for="deskripsi" class="form-label">Deskripsi</label>
+                    <input type="text" class="form-control @error('deskripsi') is-invalid @enderror" id="deskripsi" name="deskripsi" value="{{ old('deskripsi') }}">
+                    @error('deskripsi')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="ketua_divisi" class="form-label">Ketua Divisi</label>
+                    <input type="text" class="form-control @error('ketua_divisi') is-invalid @enderror" id="ketua_divisi" name="ketua_divisi" value="{{ old('ketua_divisi') }}">
+                    @error('ketua_divisi') 
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                @enderror   
+                        
+                
+                                </div>
+                <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Simpan Data</button>
+                <a href="{{ route('index') }}" class="btn btn-secondary">Kembali</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-tampilan>
