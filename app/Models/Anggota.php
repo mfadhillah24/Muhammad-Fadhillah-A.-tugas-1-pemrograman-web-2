e42a0d72328018152ea561c80c42c7ceba3234fc
mfadhillah <mfadhillah24@mhs.unitama.ac.id>
2026-05-07 18:11:18 +0800

    feat: penambahan field baru dan soft deletes pada tabel anggota

--- app/Models/Anggota.php
+++ app/Models/Anggota.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use App\Models\Divisi; 
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-#[Fillable(['nama', 'nia', 'status', 'nama_unix', 'alamat', 'no_telp', 'divisi_id'])]
+#[Fillable(['nama', 'nia', 'status', 'nama_unix', 'alamat', 'no_telp', 'divisi_id','email'])]
 
 class Anggota extends Model
 {
     /** @use HasFactory<\Database\Factories\AnggotaFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     public function divisi()
     {
         return $this->belongsTo(Divisi::class);
---
+++ database/migrations/2026_05_07_095007_add_soft_deletes_and_email_to_anggota_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('anggotas', function (Blueprint $table) {
+            $table->string('email')->nullable()->after('no_telp');
+            $table->softDeletes();
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('anggotas', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropSoftDeletes();
+        });
+    }
+};
